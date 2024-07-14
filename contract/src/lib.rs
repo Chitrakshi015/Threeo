1b8b57e63f0e0b150e78a98592d7ee4fb9693857
Frank Dierolf <frank_dierolf@web.de>
2024-07-14 03:03:25 +0200

    :zap: (contract) simplify ^^

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -3,14 +3,11 @@
 use gstd::{msg, prelude::*};
 
 // Define a static mutable variable `COUNTER` to store the click count. It is initially uninitialized.
-static mut COUNTER: Option<u128> = None;
 static mut LINK: Option<String> = None;
 
-// The `init` function is called once when the contract is deployed. It initializes the `COUNTER` to 0.
 #[no_mangle]
 extern fn init() {
     unsafe {
-        COUNTER = Some(0);
         LINK = Some(String::from("Foxy"));
     }
 }
@@ -25,15 +22,10 @@ extern fn handle() {
     // Check if the message is "Click".
     if new_msg == "Click" {
         // Safely access and modify the `COUNTER`.
-        let counter = unsafe {
-            COUNTER.as_mut().expect("Unexpected uninitialized `COUNTER`.")
-        };
         let link = unsafe{
             LINK.as_mut().expect("Unexpected uninitialized `LINK`.")
         };
         *link += "a";
-        *counter += 1; 
-                       
         // Send a reply message with "Success".
         msg::reply_bytes("Success", 0).expect("Unable to reply");
     }
@@ -43,44 +35,6 @@ extern fn handle() {
 #[no_mangle]
 extern fn state() {
     // Reply with the current value of `COUNTER`. Clone the value to ensure it is not modified by the `handle` function.
-    msg::reply(unsafe { COUNTER.clone().expect("Unexpected uninitialized `COUNTER`.") }, 0)
+    msg::reply(unsafe { LINK.clone().expect("Unexpected uninitialized `LINK`.") }, 0)
         .expect("Failed to encode or reply with `<HamsterMetadata as Metadata>::State` from `state()`");
-}
-
-// Test module to verify the functionality of the contract.
-#[cfg(test)]
-mod tests {
-    extern crate std;
-
-    use gstd::{Encode, String};
-    use gtest::{Log, Program, System};
-
-    // A test case to validate the contract's behavior.
-    #[test]
-    fn it_works() {
-        // Initialize a new system for testing.
-        let system = System::new();
-        system.init_logger();
-
-        // Initialize the current program instance.
-        let program = Program::current_opt(&system);
-
-        // Send an "INIT" message to initialize the contract.
-        let res = program.send_bytes(10, "INIT");
-        assert!(!res.main_failed());
-
-        // Read the initial state of the counter. It should be 0.
-        let counter: u128 = program.read_state(0).expect("Unexpected invalid state.");
-        assert_eq!(counter, 0);
-
-        // Send a "Click" message to the contract.
-        let res = program.send_bytes(10, String::from("Click").encode());
-        // Verify that the reply message contains "Success".
-        let log = Log::builder().source(1).dest(10).payload_bytes("Success");
-        assert!(res.contains(&log));
-
-        // Read the state of the counter again. It should be incremented to 1.
-        let counter: u128 = program.read_state(0).expect("Unexpected invalid state.");
-        assert_eq!(counter, 1);
-    }
 }
